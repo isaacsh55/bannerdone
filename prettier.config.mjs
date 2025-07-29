a87157855454b8a301a80f88107b481e3818c6d7
Isaac <iscfm97@gmail.com>
2025-07-29 16:14:31 +0800

    Fix Vercel Deploy Errors

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,15 +1,31 @@
-/**
- * @type {import("prettier").Config}
- * Need to restart IDE when changing configuration
- * Open the command palette (Ctrl + Shift + P) and execute the command > Reload Window.
- */
-const config = {
-  semi: true,
-  tabWidth: 2,
-  endOfLine: 'lf',
-  printWidth: 100,
-  singleQuote: true,
-  trailingComma: 'es5',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  trailingSlash: true,
+
+  env: {
+    BUILD_STATIC_EXPORT: 'false',
+  },
+
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/lab': {
+      transform: '@mui/lab/{{member}}',
+    },
+  },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  output: 'standalone',
 };
 
-export default config;
+export default nextConfig;
